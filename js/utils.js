a024908aadf2f14dc2415336362d1a8332b5ed36
yaz_uaz <zayarnyy74@gmail.com>
2019-10-02 18:43:49 +0500

    📦 NEW: Loading wizards from server

--- js/utils.js
+++ js/utils.js
@@ -3,6 +3,17 @@
 (function () {
   window.utils = {
     ESC_KEYCODE: 27,
-    ENTER_KEYCODE: 13
+    ENTER_KEYCODE: 13,
+    onError: function (errorMessage) {
+      var node = document.createElement('div');
+      node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+      node.style.position = 'absolute';
+      node.style.left = 0;
+      node.style.right = 0;
+      node.style.fontSize = '30px';
+
+      node.textContent = errorMessage;
+      document.body.insertAdjacentElement('afterbegin', node);
+    }
   };
 })();
